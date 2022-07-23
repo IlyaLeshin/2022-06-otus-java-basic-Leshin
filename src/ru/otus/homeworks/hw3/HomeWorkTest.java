420cfcf6ef078aeea095c8f8675f62832d0a5eab
IlyaLeshin <ilya139@list.ru>
2022-07-23 21:32:12 +0300

    Доработанное ДЗ №3 Тест (на проверку)

--- src/ru/otus/homeworks/hw3/HomeWorkTest.java
+++ src/ru/otus/homeworks/hw3/HomeWorkTest.java
@@ -6,47 +6,67 @@ public class HomeWorkTest {
     public static void main(String[] args) {
         int correctCount = 0, wrongCount = 0;
 
-        String[] questions = {
-                "Выберите номер варианта А1?",
-                "Выберите номер варианта B2?",
-                "Выберите номер варианта C2?",
-                "Выберите номер варианта D3?"
+        String questionOne = "Выберете правильный номер варианта ответа (А5Q1 is correct)";
+        String questionTwo = "Выберете правильный номер варианта ответа (А2Q2 is correct) ?";
+        String questionThree = "Выберете правильный номер варианта ответа (А1Q3 is correct) ?";
+
+        String answerFirstOptionForQuestionOne = "А1Q1";
+        String answerSecondOptionForQuestionOne = "А2Q1";
+        String answerThirdOptionForQuestionOne = "А3Q1";
+        String answerFourthOptionForQuestionOne = "А4Q1";
+        String answerFifthOptionForQuestionOne = "А5Q1";
+
+        String answerFirstOptionForQuestionTwo = "А1Q2";
+        String answerSecondOptionForQuestionTwo = "А2Q2";
+        String answerThirdOptionForQuestionTwo = "А3Q2";
+
+        String answerFirstOptionForQuestionThree = "А1Q3";
+        String answerSecondOptionForQuestionThree = "А2Q3";
+        String answerThirdOptionForQuestionThree = "А3Q3";
+        String answerFourthOptionForQuestionThree = "А4Q3";
+
+        String correctAnswerForQuestionOne = answerFifthOptionForQuestionOne;
+        String correctAnswerForQuestionTwo = answerSecondOptionForQuestionTwo;
+        String correctAnswerForQuestionThree = answerFirstOptionForQuestionThree;
+
+        String[][] questionsAndAnswerOptionsAndCorrectAnswers = {
+                {questionOne, answerFirstOptionForQuestionOne, answerSecondOptionForQuestionOne, answerThirdOptionForQuestionOne, answerFourthOptionForQuestionOne, answerFifthOptionForQuestionOne, correctAnswerForQuestionOne},
+                {questionTwo, answerFirstOptionForQuestionTwo, answerSecondOptionForQuestionTwo, answerThirdOptionForQuestionTwo, correctAnswerForQuestionTwo},
+                {questionThree, answerFirstOptionForQuestionThree, answerSecondOptionForQuestionThree, answerThirdOptionForQuestionThree, answerFourthOptionForQuestionThree, correctAnswerForQuestionThree}
         };
 
-        String[][] answerOptions = {
-                {"A1", "A2", "A3", "A4"},
-                {"B1", "B2", "B3", "B4"},
-                {"C1", "C2", "C3", "C4"},
-                {"D1", "D2", "D3", "D4"}
-        };
+        for (int i = 0; i < questionsAndAnswerOptionsAndCorrectAnswers.length; i++) {
+            String question = questionsAndAnswerOptionsAndCorrectAnswers[i][0];
+            System.out.println("(" + (i + 1) + ")" + question);
 
-        int[] correctAnswers = {0, 1, 1, 2};
-
-        for (int i = 0; i < questions.length; i++) {
-            System.out.println();
-            System.out.println("(" + (i + 1) + ")" + questions[i]);
-            System.out.println();
-
-            for (int j = 0; j < answerOptions.length; j++) {
-                System.out.println((j + 1) + ". Option: " + answerOptions[i][j]);
+            for (int j = 1; j < questionsAndAnswerOptionsAndCorrectAnswers[i].length - 1; j++) {
+                System.out.println(j + ".вариант:	" + questionsAndAnswerOptionsAndCorrectAnswers[i][j]);
             }
-            System.out.println();
-            System.out.print("Ваш ответ: ");
+            System.out.print("Ваш ответ: №");
 
             Scanner scanner = new Scanner(System.in);
-            int answerOption = scanner.nextInt() - 1;
+            while (true) {
+                int answerOption = scanner.nextInt();
 
-            if (answerOption == correctAnswers[i]) {
-                System.out.println("Excellent! Right answer is: " + correctAnswers[i]);
-                correctCount++;
-            } else {
-                System.out.println("Wrong! Right answer is: " + correctAnswers[i]);
-                wrongCount++;
+                if (answerOption < (questionsAndAnswerOptionsAndCorrectAnswers[i].length - 1) && answerOption > 0) {
+
+                    String correctAnswer = questionsAndAnswerOptionsAndCorrectAnswers[i][questionsAndAnswerOptionsAndCorrectAnswers[i].length - 1];
+
+                    if (questionsAndAnswerOptionsAndCorrectAnswers[i][answerOption].equals(correctAnswer)) {
+                        System.out.println("Великолепно! Это правильный ответ");
+                        correctCount++;
+                    } else {
+                        System.out.println("Неверно! Правильный ответ: " + correctAnswer);
+                        wrongCount++;
+                    }
+                    break;
+                } else {
+                    System.out.println("Вы ошиблись при вводе, попробуйте еще раз");
+                    System.out.print("Ваш ответ: №");
+                }
             }
-            System.out.println();
-            System.out.println("---------------------");
+            System.out.println("===	===	===	===	===");
         }
-
         System.out.println("Результат: правильно " + correctCount + ", неправильно " + wrongCount);
     }
 }
