fca77a28298a6ce9a615a20e376c71961f0146eb
IlyaLeshin <ilya139@list.ru>
2022-07-30 12:01:39 +0300

    Доработанное ДЗ №3 Тест (доп.пункт №2 "правильные ответы" хранятся как Integer в той же структуре)

--- src/ru/otus/homeworks/hw3/HomeWorkTest.java
+++ src/ru/otus/homeworks/hw3/HomeWorkTest.java
@@ -6,73 +6,69 @@ public class HomeWorkTest {
     public static void main(String[] args) {
         int correctCount = 0, wrongCount = 0;
 
-        String questionOne = "Выберете правильный номер варианта ответа (51 is correct)";
-        String questionTwo = "Выберете правильный номер варианта ответа (22 is correct)";
-        String questionThree = "Выберете правильный номер варианта ответа (13 is correct)";
+        String questionOne = "Выберете правильный номер варианта ответа (А5Q1 is correct)";
+        String questionTwo = "Выберете правильный номер варианта ответа (А2Q2 is correct) ?";
+        String questionThree = "Выберете правильный номер варианта ответа (А1Q3 is correct) ?";
 
-        int answerFirstOptionForQuestionOne = 11;
-        int answerSecondOptionForQuestionOne = 21;
-        int answerThirdOptionForQuestionOne = 31;
-        int answerFourthOptionForQuestionOne = 41;
-        int answerFifthOptionForQuestionOne = 51;
+        String answerFirstOptionForQuestionOne = "А1Q1";
+        String answerSecondOptionForQuestionOne = "А2Q1";
+        String answerThirdOptionForQuestionOne = "А3Q1";
+        String answerFourthOptionForQuestionOne = "А4Q1";
+        String answerFifthOptionForQuestionOne = "А5Q1";
 
-        int answerFirstOptionForQuestionTwo = 12;
-        int answerSecondOptionForQuestionTwo = 22;
-        int answerThirdOptionForQuestionTwo = 32;
+        String answerFirstOptionForQuestionTwo = "А1Q2";
+        String answerSecondOptionForQuestionTwo = "А2Q2";
+        String answerThirdOptionForQuestionTwo = "А3Q2";
 
-        int answerFirstOptionForQuestionThree = 13;
-        int answerSecondOptionForQuestionThree = 23;
-        int answerThirdOptionForQuestionThree = 33;
-        int answerFourthOptionForQuestionThree = 43;
+        String answerFirstOptionForQuestionThree = "А1Q3";
+        String answerSecondOptionForQuestionThree = "А2Q3";
+        String answerThirdOptionForQuestionThree = "А3Q3";
+        String answerFourthOptionForQuestionThree = "А4Q3";
 
-        String[] questions = {questionOne};
+        int numberCorrectAnswerForQuestionOne = 5;
+        int numberCorrectAnswerForQuestionTwo = 2;
+        int numberCorrectAnswerForQuestionThree = 1;
 
-        String[] newQuestions = new String[questions.length + 2];
-        for (int i = 0; i < questions.length; i++) {
-            newQuestions[i] = questions[i];
-        }
-        newQuestions[questions.length] = questionTwo;
-        newQuestions[questions.length + 1] = questionThree;
-
-        questions = newQuestions;
-
-        int[][][] answerOptionsAndCorrectAnswers = {
+        Object[][][] questionsAndAnswerOptionsAndCorrectAnswers = {
                 {
+                        {questionOne},
                         {answerFirstOptionForQuestionOne, answerSecondOptionForQuestionOne, answerThirdOptionForQuestionOne, answerFourthOptionForQuestionOne, answerFifthOptionForQuestionOne},
-                        {answerFifthOptionForQuestionOne}
+                        {numberCorrectAnswerForQuestionOne}
                 },
                 {
+                        {questionTwo},
                         {answerFirstOptionForQuestionTwo, answerSecondOptionForQuestionTwo, answerThirdOptionForQuestionTwo},
-                        {answerSecondOptionForQuestionTwo}
+                        {numberCorrectAnswerForQuestionTwo}
                 },
                 {
+                        {questionThree},
                         {answerFirstOptionForQuestionThree, answerSecondOptionForQuestionThree, answerThirdOptionForQuestionThree, answerFourthOptionForQuestionThree},
-                        {answerFirstOptionForQuestionThree}
+                        {numberCorrectAnswerForQuestionThree}
                 }
         };
 
-        for (int i = 0; i < questions.length; i++) {
-            String question = questions[i];
+        for (int i = 0; i < questionsAndAnswerOptionsAndCorrectAnswers.length; i++) {
+            Object question = questionsAndAnswerOptionsAndCorrectAnswers[i][0][0];
             System.out.println("(" + (i + 1) + ")" + question);
 
-            for (int j = 0; j < answerOptionsAndCorrectAnswers[i][0].length; j++) {
-                System.out.println((j + 1) + ".вариант:	" + answerOptionsAndCorrectAnswers[i][0][j]);
+            for (int j = 0; j < questionsAndAnswerOptionsAndCorrectAnswers[i][1].length; j++) {
+                System.out.println((j + 1) + ".вариант:	" + questionsAndAnswerOptionsAndCorrectAnswers[i][1][j]);
             }
             System.out.print("Ваш ответ: №");
 
             Scanner scanner = new Scanner(System.in);
             while (true) {
-                int answerOption = scanner.nextInt() - 1;
+                int answerOption = scanner.nextInt();
 
-                if (answerOption < (answerOptionsAndCorrectAnswers[i][0].length) && answerOption >= 0) {
+                if (answerOption <= (questionsAndAnswerOptionsAndCorrectAnswers[i][1].length) && answerOption > 0) {
 
-                    int correctAnswer = answerOptionsAndCorrectAnswers[i][1][0];
+                    int numberCorrectAnswer = (Integer) questionsAndAnswerOptionsAndCorrectAnswers[i][2][0];
 
-                    if (answerOptionsAndCorrectAnswers[i][0][answerOption] == correctAnswer) {
+                    if (answerOption == numberCorrectAnswer){
                         System.out.println("Великолепно! Это правильный ответ");
                         correctCount++;
-                    } else {
-                        System.out.println("Неверно! Правильный ответ: " + correctAnswer);
+                    } else{
+                        System.out.println("Неверно! Правильный ответ: " + numberCorrectAnswer);
                         wrongCount++;
                     }
                     break;
