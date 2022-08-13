61b0413ca46e4db40abcf3e027a684152f886b90
IlyaLeshin <ilya139@list.ru>
2022-08-13 14:22:58 +0300

    ДЗ №4 OOP (корректировка 1) классы с записью/выводом вопросов(Question), вариантов ответов(AnswerOptions) и корректных ответов(NumCorrectAnswer) объеденены в один(QuestionAndAnswersOptionsAndCorrectAnswer.java)  вынесен в отдельный файлы, так же как и счетчик (Count.java),

---
+++ src/ru/otus/homeworks/hw4/Count.java
@@ -0,0 +1,17 @@
+package ru.otus.homeworks.hw4;
+
+class Count {
+    private int count;
+
+    public Count(int count) {
+        this.count = count;
+    }
+
+    public void plusCount() {
+        count++;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
--- src/ru/otus/homeworks/hw4/HomeWorkOOP.java
+++ src/ru/otus/homeworks/hw4/HomeWorkOOP.java
@@ -9,37 +9,29 @@ public class HomeWorkOOP {
         Count correctCount = new Count(0);
         Count wrongCount = new Count(0);
 
-        Question questionOne = new Question("Выберете правильный номер варианта ответа (А5Q1 is correct)");
-        Question questionTwo = new Question("Выберете правильный номер варианта ответа (А2Q2 is correct)");
-        Question questionThree = new Question("Выберете правильный номер варианта ответа (А1Q3 is correct)");
+        QuestionAndAnswersOptionsAndCorrectAnswer questionOne = new QuestionAndAnswersOptionsAndCorrectAnswer(
+                "Выберете правильный номер варианта ответа (А5Q1 is correct)",
+                new String[]{"А1Q1", "А2Q1", "А3Q1", "А4Q1", "А5Q1"},
+                5);
+        QuestionAndAnswersOptionsAndCorrectAnswer questionTwo = new QuestionAndAnswersOptionsAndCorrectAnswer(
+                "Выберете правильный номер варианта ответа (А2Q2 is correct)",
+                new String[]{"А1Q2", "А2Q2", "А3Q2"},
+                2);
 
-        ArrayList<Question> questionsArr = new ArrayList<>();
+        QuestionAndAnswersOptionsAndCorrectAnswer questionThree = new QuestionAndAnswersOptionsAndCorrectAnswer(
+                "Выберете правильный номер варианта ответа (А1Q3 is correct)",
+                new String[]{"А1Q3", "А2Q3", "А3Q3", "А4Q3"},
+                1);
+
+        ArrayList<QuestionAndAnswersOptionsAndCorrectAnswer> questionsArr = new ArrayList<>();
         questionsArr.add(questionOne);
         questionsArr.add(questionTwo);
         questionsArr.add(questionThree);
 
-        AnswerOptions answersForQuestionOne = new AnswerOptions(new String[]{"А1Q1", "А2Q1", "А3Q1", "А4Q1", "А5Q1"});
-        AnswerOptions answersForQuestionTwo = new AnswerOptions(new String[]{"А1Q2", "А2Q2", "А3Q2"});
-        AnswerOptions answersForQuestionThree = new AnswerOptions(new String[]{"А1Q3", "А2Q3", "А3Q3", "А4Q3"});
-
-        ArrayList<AnswerOptions> answersArr = new ArrayList<>();
-        answersArr.add(answersForQuestionOne);
-        answersArr.add(answersForQuestionTwo);
-        answersArr.add(answersForQuestionThree);
-
-        NumCorrectAnswer numCorrectAnswerFoQuestionOne = new NumCorrectAnswer(5);
-        NumCorrectAnswer numCorrectAnswerFoQuestionTwo = new NumCorrectAnswer(2);
-        NumCorrectAnswer numCorrectAnswerFoQuestionThree = new NumCorrectAnswer(1);
-
-        ArrayList<NumCorrectAnswer> numCorrectAnswersArr = new ArrayList<>();
-        numCorrectAnswersArr.add(numCorrectAnswerFoQuestionOne);
-        numCorrectAnswersArr.add(numCorrectAnswerFoQuestionTwo);
-        numCorrectAnswersArr.add(numCorrectAnswerFoQuestionThree);
-
         for (int i = 0; i < questionsArr.size(); i++) {
             System.out.println("Вопрос №"+(i+1)+": "+questionsArr.get(i).getQuestion());
-            for (int j = 0; j < answersArr.get(i).getAnswerOptions().length; j++) {
-                System.out.println("Вариант ответа №" + (j + 1) + ": " + answersArr.get(i).getAnswerOptions()[j]);
+            for (int j = 0; j < questionsArr.get(i).getAnswerOptions().length; j++) {
+                System.out.println("Вариант ответа №" + (j + 1) + ": " + questionsArr.get(i).getAnswerOptions()[j]);
             }
 
             System.out.print("Ваш ответ: №");
@@ -47,12 +39,12 @@ public class HomeWorkOOP {
 
             while (true) {
                 Integer answerOption = scanner.nextInt();
-                if (answerOption <= (answersArr.get(i).lengthAnswerOptions()) && answerOption > 0) {
-                    if (answerOption.equals(numCorrectAnswersArr.get(i).getNumCorrectAnswer())) {
+                if (answerOption <= (questionsArr.get(i).lengthAnswerOptions()) && answerOption > 0) {
+                    if (answerOption.equals(questionsArr.get(i).getNumCorrectAnswer())) {
                         System.out.println("Великолепно! Это правильный ответ");
                         correctCount.plusCount();
                     } else {
-                        System.out.println("Неверно! Правильный ответ: " + numCorrectAnswersArr.get(i).getNumCorrectAnswer());
+                        System.out.println("Неверно! Правильный ответ: " + questionsArr.get(i).getNumCorrectAnswer());
                         wrongCount.plusCount();
                     }
                     break;
@@ -67,62 +59,4 @@ public class HomeWorkOOP {
     }
 }
 
-
-class Count {
-    private int count;
-
-    public Count(int count) {
-        this.count = count;
-    }
-
-    public void plusCount() {
-        count++;
-    }
-
-    public int getCount() {
-        return count;
-    }
-}
-
-class Question {
-    private String question;
-
-    public Question(String question) {
-        this.question = question;
-    }
-
-    public String getQuestion() {
-        return question;
-    }
-
-}
-
-class AnswerOptions {
-    private String[] answerOptions;
-
-    public AnswerOptions(String[] answerOptions) {
-        this.answerOptions = answerOptions;
-    }
-
-    public String[] getAnswerOptions() {
-        return answerOptions;
-    }
-
-    public Integer lengthAnswerOptions() {
-        return answerOptions.length;
-    }
-
-}
-
-class NumCorrectAnswer {
-    private Integer NumCorrectAnswer;
-
-    public NumCorrectAnswer(Integer numCorrectAnswer) {
-        this.NumCorrectAnswer = numCorrectAnswer;
-    }
-
-    public Integer getNumCorrectAnswer() {
-        return NumCorrectAnswer;
-    }
-}
 
