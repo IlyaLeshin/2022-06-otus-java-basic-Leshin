52de2f38dace0ebded6e7aff5ce5fb9039b6ab55
IlyaLeshin <ilya139@list.ru>
2022-08-13 13:19:23 +0300

    ДЗ №5 Сортировка чисел (рабочая версия)
    Сравнивается время выполнения Collections.sort и пузырьковой сортировки
    Так же применена оптимизировнная пузырьковая сортировка, где при каждой итерации уменьшается область сортировки и добавлен выход если на проходе не было перестановок

---
+++ src/ru/otus/homeworks/hw5/BubbleSort.java
@@ -0,0 +1,64 @@
+package ru.otus.homeworks.hw5;
+
+import java.util.List;
+
+class BubbleSort {
+    protected static void BubbleSortVerClassic(List<Integer> array) {
+        int tempValue;
+
+           for (int k = array.size() - 1; k > 0; k--) {
+
+            for (int i = 0; i < k; i++) {
+                if (array.get(i) > array.get(i + 1)) {
+                    tempValue = array.get(i);
+                    array.set((i), array.get(i + 1));
+                    array.set(i + 1, tempValue);
+                }
+            }
+        }
+    }
+    protected static void BubbleSortVerChangeMaxAndMinPosition(List<Integer> array) {
+        int tempValue;
+        int currentPosition;
+        int maxPosition;
+        int changedMaxPosition = array.size() - 1;
+        int minPosition = 0;
+        boolean noChanges;
+
+        for (maxPosition = array.size() - 1; maxPosition >= 0; minPosition++) {
+            noChanges = true;
+
+            for (currentPosition = minPosition; currentPosition < maxPosition; currentPosition++) {
+                if (array.get(currentPosition) > array.get(currentPosition + 1)) {
+                    tempValue = array.get(currentPosition + 1);
+                    array.set(currentPosition + 1, array.get(currentPosition));
+                    array.set(currentPosition, tempValue);
+
+                    noChanges = false;
+                    changedMaxPosition = currentPosition;
+                }
+                if (array.get(currentPosition) < array.get(minPosition)) {
+                    tempValue = array.get(minPosition);
+                    array.set(minPosition, array.get(currentPosition));
+                    array.set(currentPosition, tempValue);
+
+                    noChanges = false;
+                }
+                if (array.get(currentPosition) > array.get(maxPosition)) {
+                    tempValue = array.get(maxPosition);
+                    array.set(maxPosition, array.get(currentPosition));
+                    array.set(currentPosition, tempValue);
+
+                    noChanges = false;
+                }
+            }
+            if (noChanges) {
+                break;
+            }
+
+            maxPosition = changedMaxPosition;
+        }
+
+    }
+
+}
